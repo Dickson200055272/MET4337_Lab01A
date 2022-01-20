8cafe36eacee04e1d69fb65fc2e5f457e4086ad0
Dickson200055272 <200055272@stu.vtc.edu.hk>
2022-01-20 16:05:15 +0800

    ReNew
    New_Restart
    Phase 1

--- Assets/Scripts/Managers/GameManagers.cs
+++
@@ -1,8 +0,0 @@
-public class GameManagers : Singleton<GameManagers>
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        DontDestroyOnLoad(this.transform.parent);
-    }
-}
